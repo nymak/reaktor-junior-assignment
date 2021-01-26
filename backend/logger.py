229787dede85b2cba76c9ee6e6bfd6c22b516596
Markus Nyman <markus@markusn.dev>
2021-01-26 15:48:38 +0200

    Log to file with time

--- backend/logger.py
+++ backend/logger.py
@@ -7,12 +7,15 @@ async def on_request_start(session, context, params):
     logging.getLogger('aiohttp.client').debug(f'Starting request <{params}>')
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    filename='logs.log',
+    level=logging.DEBUG,
+    format="%(asctime)s %(message)s",
+)
 trace_config = TraceConfig()
 trace_config.on_request_start.append(on_request_start)
 
 http.client.HTTPConnection.debuglevel = 1
 
-logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
